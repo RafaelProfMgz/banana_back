540e96ab4c2cf5a189b4f6ef904dbea056c48487
RafaelProfMgz <rafaelprojmgz@gmail.com>
2025-10-16 21:37:02 -0400

    feat: adicionado modulos necessarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupabaseModule } from './supabase/supabase.module';
+import { ChatModule } from './chat/chat.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [SupabaseModule, ChatModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
